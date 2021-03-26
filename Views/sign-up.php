518473215965dc6ce8b33b33c98be9814c5e2cc7
imadeekosatria <imadeekosatriawiguna@gmail.com>
2021-03-26 22:47:09 +0800

    Form-Rincian Sisa Admin

--- Views/sign-up.php
+++ Views/sign-up.php
@@ -69,5 +69,14 @@
             </div>
         </div>
     </section>
+    <script> 
+        function onlyNumberKey(evt) { 
+            // Only ASCII charactar in that range allowed 
+            var ASCIICode = (evt.which) ? evt.which : evt.keyCode 
+            if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) 
+            return false; 
+            return true; 
+        } 
+    </script>
 </body>
 </html>
