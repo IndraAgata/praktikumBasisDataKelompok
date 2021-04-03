aabd589e6f37d7656abcaa45a84c5bd5c8d379f0
imadeekosatria <imadeekosatriawiguna@gmail.com>
2021-04-03 10:08:28 +0800

    Rincian Pinjam

--- Views/Rincian Pinjam.php
+++ Views/Rincian Pinjam.php
@@ -1,3 +1,14 @@
+<?php
+    include("../php/db.con.php");
+    include("../php/session.php");
+    $tampil = mysqli_query($conn, "SELECT username, concat(firstname, ' ', lastname) AS Nama FROM data_pengguna WHERE username = '$user'");
+    $data = mysqli_fetch_array($tampil);
+?>
+
+<?php
+    $display = mysqli_query($conn, "SELECT * FROM rincian_pinjam WHERE username = '$user'");
+    $rincian = mysqli_fetch_array($display);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,12 +36,12 @@
                     </div>
                     <div class="col-lg-2">
                         <h1 class="judul">Koperasi</h1>
-                        <h4 class="judul-user">Nama</h4>
+                        <h4 class="judul-user"><?php echo $data['username'];?></h4>
                     </div>
                 </div>
             </div>
             <div class="col-lg-8 mx-5">
-                <a href=""><h4 class="logout">Logout</h4></a>
+                <a href="../php/logout.php"><h4 class="logout">Logout</h4></a>
             </div>
         </div>
         <section class="Form my-4 mx-5">
@@ -41,12 +52,12 @@
                             <h5 class="judul-user">Data Diri</h5> 
                             <div class="form-row justify-content-center">
                                 <div class="col-lg-10 mt-3">
-                                    <input class="form-control" type="text" name="username" placeholder="Username" disabled readonly>
+                                    <input class="form-control" type="text" name="username" value="<?php echo $data['username'];?>" placeholder="Username" disabled readonly>
                                 </div>
                             </div>
                             <div class="form-row justify-content-center">
                                 <div class="col-lg-10 mt-3">
-                                    <input class="form-control" type="text" name="nama" placeholder="Nama" disabled readonly>
+                                    <input class="form-control" type="text" name="nama" value="<?php echo $data['Nama'];?>"placeholder="Nama" disabled readonly>
                                 </div>
                             </div>                      
                         </div>
@@ -57,7 +68,7 @@
                             </div>
                             <div class="form-row justify-content-center">
                                 <div class="col-lg-8">
-                                    <input class="form-control"  type="date" name="tglpinjam" disabled readonly>
+                                    <input class="form-control"  type="date" value="<?php echo $rincian['tanggalpinjam']?>" name="tglpinjam" disabled readonly>
                                 </div>
                             </div>
                             <div class="row mt-3 justify-content-center">
@@ -65,27 +76,27 @@
                             </div>
                             <div class="form-row justify-content-center">
                                 <div class="col-lg-8">
-                                    <input class="form-control"  type="date" name="tglbayar" disabled readonly>
+                                    <input class="form-control"  type="date" value="<?php echo $rincian['tanggalbayar']?>" name="tglbayar" disabled readonly>
                                 </div>
                             </div>
                             <div class="form-row justify-content-center">
                                 <div class="col-lg-7 mt-3">
-                                    <input class="form-control" onkeypress="return onlyNumberKey(event)" type="text" name="jumlah" placeholder="Jumlah Dana" disabled readonly>
+                                    <input class="form-control" onkeypress="return onlyNumberKey(event)" type="text" value="<?php echo $rincian['jumlah']?>" name="jumlah" placeholder="Jumlah Dana" disabled readonly>
                                     
                                 </div>
                                 <div class="col-lg-4 mx-3 mt-3">
-                                    <input class="form-control" onkeypress="return onlyNumberKey(event)" type="text" name="bunga" placeholder="Bunga" disabled readonly>
+                                    <input class="form-control" onkeypress="return onlyNumberKey(event)" type="text" value="<?php echo$rincian['bunga']?>" name="bunga" placeholder="Bunga" disabled readonly>
                                 </div>
                             </div>
                             <div class="form-row justify-content-center">
                                 <div class="col-lg-10 mt-3">
-                                    <input class="form-control" type="text" name="total" placeholder="Total Bayar" disabled readonly>
+                                    <input class="form-control" type="text" name="total" value="<?php echo$rincian['jumlah']?>" placeholder="Total Bayar" disabled readonly>
                                 </div>
                             </div>      
                         </div>
                         <div class="row p-5">
                             <div class="col">
-                                <a href="homepage.html"><button class="btn1">Kembali</button></a>
+                                <a href="homepage.php"><button class="btn1">Kembali</button></a>
                             </div>
                         </div>
                     </form>
